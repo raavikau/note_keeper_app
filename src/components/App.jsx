8e4fc523d5c883260e20f9591360e4c5db9d1d79
Raavi Kaur <raavi211299@gmail.com>
2024-12-06 17:36:23 -0600

    add the basic code in the app file and export to index file

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+    return <div>
+        <header>
+            <h1>Note Keeper</h1>
+        </header>
+        <div className="note">
+            <h1>This is the title</h1>
+            <p>This is the note content</p>
+        </div>
+    </div>
+}
+
+export default App;
