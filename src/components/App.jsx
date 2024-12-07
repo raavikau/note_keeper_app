c7fe537d042a8dbe420b16e0192505635ad7b998
Raavi Kaur <raavi211299@gmail.com>
2024-12-07 12:56:34 -0600

    added the maping function to iterate over notes array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Notes from "./Notes";
 import Footer from "./Footer";
-import note from "../notes";
+import notes from "../notes";
 
+function createNote(note){
+    return  (<Notes 
+        key={note.key}
+        title={note.title} 
+        content={note.content} 
+    /> );
+}
 function App(){
     return <div>
         <Header />
-        <Notes title={note[0].title} content={note[0].content} />
-        <Notes title={note[1].title} content={note[1].content} />
-        <Notes title={note[2].title} content={note[2].content} />
-        <Notes title={note[3].title} content={note[3].content} />
-        <Notes title={note[4].title} content={note[4].content} />
+        {notes.map(createNote)}
         <Footer />
     </div>
 }
