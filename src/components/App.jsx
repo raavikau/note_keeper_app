21f38c2cfc76edf384f03b2327f006f2a95d91ab
Raavi Kaur <raavi211299@gmail.com>
2024-12-07 12:43:23 -0600

    use of props that display notes from the js file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Notes from "./Notes";
 import Footer from "./Footer";
+import note from "../notes";
 
 function App(){
     return <div>
         <Header />
-        <Notes />
+        <Notes title={note[0].title} content={note[0].content} />
+        <Notes title={note[1].title} content={note[1].content} />
+        <Notes title={note[2].title} content={note[2].content} />
+        <Notes title={note[3].title} content={note[3].content} />
+        <Notes title={note[4].title} content={note[4].content} />
         <Footer />
     </div>
 }
---
+++ src/notes.js
@@ -0,0 +1,35 @@
+const notes = [
+    {
+      key: 1,
+      title: "Software",
+      content:
+        "Software is a set of instructions, data or programs used to operate computers and execute specific tasks. ",
+    },
+    {
+      key: 2,
+      title: "Encryption",
+      content:
+        "Encryption is the method by which information is converted into secret code that hides the information's true meaning."
+    },
+    {
+      key: 3,
+      title: "Map object",
+      content:
+        "The Map object is a data structure that allows you to store key-value pairs where the keys can be of any type.",
+    },
+    {
+      key: 4,
+      title: "Cloud computing",
+      content:
+        "Cloud computing simply refers to the delivery of computing services over the internet, including storage, databases, software, and analytics."
+    },
+    {
+      key: 5,
+      title: "Mapping",
+      content:
+        "The map function used to iterate over an array and apply a function to each item in the array, returning a new array with the modified values."
+    },
+  ];
+  
+  export default notes;
+  
