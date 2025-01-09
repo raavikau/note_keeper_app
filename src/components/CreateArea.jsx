989a21ccc5affde7386e36bfafce499f332a92f6
Raavi Kaur <raavi211299@gmail.com>
2025-01-09 12:59:55 -0600

    add code to perform submit functionality

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
 
-function CreateArea(){
-    const [note, addNote] = useState({title:"",content:""});
+function CreateArea(props) {
+    const [note, addNote] = useState({ title: "", content: "" });
+    
     function handleChange(event) {
         const {name, value} = event.target;
         addNote((previousval) => {
             return {
                 ...previousval,
-                [name] : value
+                [name] : value,
             }
         });
     }
+
+    function submitNote(event) {
+        props.onAddContent(note);
+        addNote({ title: "", content: "" });
+        event.preventDefault();
+    }
+
     return <form>
-        <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
-        <textarea onChange={handleChange} name="content" placeholder="Take a note..."  value={note.content} />
-        <button>Add</button>
+            <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+            <textarea onChange={handleChange} name="content" placeholder="Take a note..."  value={note.content} />
+            <button onClick={submitNote}>Add</button>
         </form>
 }
 
