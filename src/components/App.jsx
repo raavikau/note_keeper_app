2659b8287436589d6b0359627ef65b200d0790e3
Raavi Kaur <raavi211299@gmail.com>
2025-01-09 13:02:58 -0600

    take array and render seperate Note component fro each item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import CreateArea from "./CreateArea";
 
-function App(){
-    return <div>
-        <Header />
-        <CreateArea />
-        <Note key={1} title="Note title" content="Note Content" />
-        <Footer />
-    </div>
+function App() {
+    const [noteContent, setNote] = useState([]);
+
+    function addContent(newNote) {
+        setNote((previousval) => {
+            return [...previousval, newNote];
+        });
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea onAddContent={addContent} />
+            {noteContent.map((noteItem) => {
+                return <Note 
+                        key={1}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                    />
+            })}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
