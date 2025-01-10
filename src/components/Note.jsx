ae9c4a3ca38ab2efc462a3998f6fd4c33090b254
Raavi Kaur <raavi211299@gmail.com>
2025-01-10 13:57:57 -0600

    add code to cllback from Note component to trigger delete func

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function Note(props){
-    return <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+function Note(props) {
+    return (
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button 
+                onClick={() => {
+                    props.onDeleteItem(props.id);
+                }} 
+                > Delete
+            </button>
         </div>
+    );
 }
 
 export default Note;
